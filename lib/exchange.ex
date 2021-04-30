3ca818080ca957bbcf913e732a365300244f9a17
Lei Shi <wolfgangshi@gmail.com>
2021-04-30 21:10:30 +0800

    First draft to pass doctest

--- lib/exchange.ex
+++ lib/exchange.ex
@@ -60,19 +60,91 @@ defmodule Exchange do
     ]
   """
 
+  use GenServer
+
   @doc """
   Start link
   """
-  def start_link() do
-    {:ok, nil}
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, [], opts)
   end
 
   @spec send_instruction(pid(), map()) :: :ok
-  def send_instruction(_pid, _event) do
-    :ok
+  def send_instruction(pid, event) do
+    GenServer.call(pid, {:instr, event})
   end
 
-  def order_book(_pid, _book_depth) do
-    []
+  def order_book(pid, book_depth) do
+    GenServer.call(pid, {:order_book, book_depth})
+  end
+
+  @impl true
+  def init(_opts) do
+    {:ok, %{order_book: SimpleListOrderBook.new()}}
+  end
+
+  @impl true
+  def handle_call({:instr, event = %{instruction: :new}}, _from, state = %{order_book: ob}) do
+    updated_ob = SimpleListOrderBook.insert(ob, Map.take(event, [:side, :quantity, :price_level_index, :price]))
+    {:reply, :ok, %{state | order_book: updated_ob}}
+  end
+
+  def handle_call({:instr, event = %{instruction: :update}}, _from, state = %{order_book: ob}) do
+    updated_ob = SimpleListOrderBook.update(ob, Map.take(event, [:side, :quantity, :price_level_index, :price]))
+    {:reply, :ok, %{state | order_book: updated_ob}}
+  end
+
+  def handle_call({:order_book, book_depth}, _from, state = %{order_book: ob}) do
+    order_book = SimpleListOrderBook.match_order(ob, book_depth)
+    {:reply, order_book, state}
+  end
+end
+
+
+defmodule SimpleListOrderBook do
+  def new() do
+    {[], []}
+  end
+
+  def insert({bids, asks}, bid = %{price_level_index: pl_index, side: :bid}) do
+    {insert_at(bids, pl_index, bid.price, bid.quantity), asks}
+  end
+
+  def insert({bids, asks}, ask = %{price_level_index: pl_index, side: :ask}) do
+    {bids, insert_at(asks, pl_index, ask.price, ask.quantity)}
+  end
+
+  def update({bids, asks}, bid = %{price_level_index: pl_index, side: :bid}) do
+    {update_at(bids, pl_index, bid.price, bid.quantity), asks}
+  end
+
+  def update({bids, asks}, ask = %{price_level_index: pl_index, side: :ask}) do
+    {bids, update_at(asks, pl_index, ask.price, ask.quantity)}
+  end
+
+  defp update_at(list, pl_index, price, quantity) do
+    Kernel.update_in(list, [Access.at!(pl_index - 1)], fn pl -> %{pl | q: quantity, p: price} end)
+  end
+
+  defp insert_at([], 1, price, quantity) do
+    [%{q: quantity, p: price}]
+  end
+  defp insert_at([head | tail], 1, price, quantity) do
+    [%{q: quantity, p: price}, head | tail]
+  end
+  defp insert_at([head | tail], pl_index, price, quantity) do
+    [head | insert_at(tail, pl_index - 1, price, quantity)]
+  end
+
+  def match_order({bids, asks}, order_depth) do
+    [bids, asks]
+    |> Stream.zip()
+    |> Enum.take(order_depth)
+    |> Enum.map(fn {bid, ask} -> %{
+      ask_price: ask.p,
+      ask_quantity: ask.q,
+      bid_price: bid.p,
+      bid_quantity: bid.q
+    } end)
   end
 end
